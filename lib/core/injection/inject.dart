5d33e823d81ebc908d97b0cc82d097e2cba87ece
unknown <viniciusps01@hotmail.com>
2022-04-04 10:52:33 -0300

    Cart dependency was injected

--- lib/core/injection/inject.dart
+++ lib/core/injection/inject.dart
@@ -1,17 +1,24 @@
 import 'package:get_it/get_it.dart';
 import 'package:interview_challenge/data/datasources/get_all_products_datasource.dart';
 import 'package:interview_challenge/data/datasources/remote/get_all_products_remote_datasource_imp.dart';
+import 'package:interview_challenge/data/dtos/cart_dto.dart';
 import 'package:interview_challenge/data/repositories/get_all_products_repository_imp.dart';
+import 'package:interview_challenge/domain/entities/cart_entity.dart';
 import 'package:interview_challenge/domain/repositories/get_all_products_repository.dart';
 import 'package:interview_challenge/domain/usecases/get_all_products_usecase/get_all_products_usecase.dart';
 import 'package:interview_challenge/domain/usecases/get_all_products_usecase/get_all_products_usecase_imp.dart';
 
 class Inject {
   static init() {
+    registerCart();
     initDataSources();
     initRepositories();
     initUseCases();
     initControllers();
+  }
+
+  static registerCart() {
+    GetIt.I.registerFactory<CartEntity>(() => CartDto(id: '1111', items: []));
   }
 
   static initDataSources() {
