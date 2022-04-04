49a3e818ee9fcd68600adee28d988b1c02a9af25
unknown <viniciusps01@hotmail.com>
2022-04-04 14:28:27 -0300

    Added data source for clearing cart

---
+++ lib/data/datasources/remote/clear_cart_remote_datasource_imp.dart
@@ -0,0 +1,9 @@
+import 'package:interview_challenge/data/datasources/clear_cart_datasource.dart';
+
+class ClearCartRemoteDataSourceImp implements ClearCartDataSource {
+  @override
+  Future<bool> call() async {
+    // TODO:call API
+    return true;
+  }
+}
