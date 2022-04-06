51125316996fe30fc35e1a7e650daae2b5535dfb
unknown <viniciusps01@hotmail.com>
2022-04-06 19:46:13 -0300

    Encapsulation was improved

--- lib/presentation/controllers/products_controller/products_controller.g.dart
+++ lib/presentation/controllers/products_controller/products_controller.g.dart
@@ -9,18 +9,48 @@ part of 'products_controller.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
 
 mixin _$ProductsController on _ProductsControllerBase, Store {
-  final _$productsAtom = Atom(name: '_ProductsControllerBase.products');
+  Computed<bool>? _$isLoadingComputed;
 
   @override
-  List<ProductDto>? get products {
-    _$productsAtom.reportRead();
-    return super.products;
+  bool get isLoading =>
+      (_$isLoadingComputed ??= Computed<bool>(() => super.isLoading,
+              name: '_ProductsControllerBase.isLoading'))
+          .value;
+  Computed<List<ProductDto>?>? _$productsComputed;
+
+  @override
+  List<ProductDto>? get products =>
+      (_$productsComputed ??= Computed<List<ProductDto>?>(() => super.products,
+              name: '_ProductsControllerBase.products'))
+          .value;
+
+  final _$_isLoadingAtom = Atom(name: '_ProductsControllerBase._isLoading');
+
+  @override
+  bool get _isLoading {
+    _$_isLoadingAtom.reportRead();
+    return super._isLoading;
   }
 
   @override
-  set products(List<ProductDto>? value) {
-    _$productsAtom.reportWrite(value, super.products, () {
-      super.products = value;
+  set _isLoading(bool value) {
+    _$_isLoadingAtom.reportWrite(value, super._isLoading, () {
+      super._isLoading = value;
+    });
+  }
+
+  final _$_productsAtom = Atom(name: '_ProductsControllerBase._products');
+
+  @override
+  List<ProductDto>? get _products {
+    _$_productsAtom.reportRead();
+    return super._products;
+  }
+
+  @override
+  set _products(List<ProductDto>? value) {
+    _$_productsAtom.reportWrite(value, super._products, () {
+      super._products = value;
     });
   }
 
@@ -35,6 +65,7 @@ mixin _$ProductsController on _ProductsControllerBase, Store {
   @override
   String toString() {
     return '''
+isLoading: ${isLoading},
 products: ${products}
     ''';
   }
